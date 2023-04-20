c4c7fd9825a5b224e17ee7e92c31ec8cd4f720a5
Jordan <40555230+icedoesjs@users.noreply.github.com>
2023-04-20 12:46:00 -0400

    Update usage.js

--- examples/usage.js
+++ examples/usage.js
@@ -1,7 +1,8 @@
 const SJ = require('sj-json');
 const simplified_json = new SJ();
 
-let parsed = simplified_json.parse('filename.sj');
+// The second argument is the parse function is where the txt files you require are located.
+let parsed = simplified_json.parse('filename.sj', '');
 
-// This will output the SJ parsed to JSON
+// This will output the SJ parsed to JSON.
 console.log(parsed)
