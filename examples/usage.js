54272db14f4b8cf8cc2f9d4172ea5ade39bb9038
Jordan <40555230+icedoesjs@users.noreply.github.com>
2023-04-22 23:27:42 -0400

    Add quiet mode

--- examples/usage.js
+++ examples/usage.js
@@ -1,8 +1,8 @@
 const SJ = require('sj-json');
-const simplified_json = new SJ();
+const simplified_json = new SJ(true); // Quiet mode enabled (no logs shown)
 
 // The second argument is the parse function is where the txt files you require are located.
-let parsed = simplified_json.parse('filename.sj', '');
+let parsed = simplified_json.parse('example.sj', '');
 
 // This will output the SJ parsed to JSON.
 console.log(parsed)
